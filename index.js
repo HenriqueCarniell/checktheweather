bb4556a9abb6ebeedf6577cf0f76426999c4fc76
Gustavo Henrique Queiroz Carniel <97348868+HenriqueCarniell@users.noreply.github.com>
2023-07-12 14:55:06 -0300

    index.js

--- index.js
+++ index.js
@@ -1,112 +1,80 @@
-//variaveis
-var nomedofilme = document.getElementById('input-text')
-var botao = document.getElementById('button')
-var res = document.getElementById('res')
+// Variáveis
+const apikey = "98e4fc8d5d2206697f7738fad81834ac";
 
-//funções
-async function getdados(title) {
-  var API = `https://www.omdbapi.com/?t=${title}&apikey=fa61722f`
-  const response = await fetch(API)
-  const data = await response.json()
-  return data
+const input_pais = document.getElementById('country-input');
+const botao = document.getElementById('botao');
+const lugar = document.getElementById('país');
+const graus = document.getElementById('graus');
+const ceu = document.getElementById('ceu');
+const umidade = document.getElementById('umidade');
+const velocidade = document.getElementById('velocidade');
+const flag = document.getElementById('flag');
+const ceu_flag = document.getElementById('ceu-flag');
+const info = document.getElementById('div-info');
+const pais = document.getElementById('pais');
+
+// Funções
+async function getWeather(cidade) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apikey}&lang=pt`);
+    const information = document.getElementById('information');
+
+    if (response.status === 404) {
+        const p = document.createElement('p');
+        p.textContent = 'Você deve digitar um país ou cidade válido';
+        p.style.fontSize = '15px';
+        p.style.color = 'red';
+        p.style.display = 'flex';
+        p.style.justifyContent = 'center';
+        information.innerHTML = '';
+        information.appendChild(p);
+    }
+
+    const data = await response.json();
+
+    const temperaturainKelvin = data.main.temp;
+    const temperaturainGraus = temperaturainKelvin - 273.15;
+    graus.innerHTML = temperaturainGraus.toFixed(0) + '°C';
+
+    const descricao = data.weather[0].description;
+    ceu.innerHTML = descricao;
+
+    const nome = data.name;
+    lugar.innerHTML = nome;
+    let iElementPais = lugar.querySelector('i');
+    if (!iElementPais) {
+        iElementPais = document.createElement('i');
+        lugar.insertBefore(iElementPais, lugar.firstChild);
+    }
+    iElementPais.className = 'bi bi-geo-alt-fill';
+
+    const velocidade_vento = data.wind.speed;
+    velocidade.innerHTML = velocidade_vento + 'KM';
+    const iElementVelocidade = velocidade.closest('i');
+    if (iElementVelocidade) {
+        iElementVelocidade.className = 'bi bi-wind';
+    }
+
+    const umidade_atual = data.main.humidity;
+    umidade.innerHTML = umidade_atual + '%';
+    const iElementUmidade = umidade.closest('i');
+    if (iElementUmidade) {
+        iElementUmidade.className = 'bi bi-droplet-fill';
+    }
+
+    console.log(data);
 }
 
-//eventos
-botao.addEventListener('click', async () => {
-  var response = await getdados(nomedofilme.value)
-  var genero = response.Genre
-  var arraygenero = genero.split(",")
+// Eventos
+botao.addEventListener('click', (e) => {
+    e.preventDefault();
+    const cidade = input_pais.value;
+    getWeather(cidade);
+});
 
-  if (nomedofilme.value !== '') {
-    if (response.Response === 'True') {
-      res.innerHTML = `
-      <div id="movie-content">
-        <div id="resmovie">
-          <img id="poster" src="${response.Poster}">
-        </div>
-        <div id="content2">
-          <div class="info-container">
-            <div id="title">
-              <h2 class="nomefilme">${response.Title}</h2>
-            </div>
-            <div class="rating">
-              <i id="star" class="bi bi-star-fill"></i>
-              <h4>${response.imdbRating}</h4>
-            </div>
-            <div class="description">
-              <span>${response.Rated}</span>
-              <span>${response.Year}</span>
-              <span>${response.Runtime}</span>
-            </div>
-            <div class="genero-container">
-              ${arraygenero.map(genero => `<p class="genero">${genero}</p>`).join('')}
-            </div>
-          </div>
-        </div>
-      </div>
-      <div id="description2">
-        <h2>Descrição</h2>
-        <span>${response.Plot}</span>
-      </div>
-      <div id="atores">
-        <h2>Atores</h2>
-        <span>${response.Actors}</span>
-      </div>`
-    } else {
-      res.innerHTML = 'Filme não encontrado'
+input_pais.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        const cidade = input_pais.value;
+        getWeather(cidade);
     }
-  } else {
-    res.innerHTML = 'Digite um filme válido'
-  }
-})
-
-nomedofilme.addEventListener('keydown', (e) => {
-  if(e.key === "Enter") {
-    e.preventDefault()
-    const title = nomedofilme.value
-    getdados(title).then(response => {
-      var genero = response.Genre
-      var arraygenero = genero.split(",")
-
-      if (response.Response === 'True') {
-        res.innerHTML = `
-          <div id="movie-content">
-            <div id="resmovie">
-              <img id="poster" src="${response.Poster}">
-            </div>
-            <div id="content2">
-              <div class="info-container">
-                <div id="title">
-                  <h2 class="nomefilme">${response.Title}</h2>
-                </div>
-                <div class="rating">
-                  <i id="star" class="bi bi-star-fill"></i>
-                  <h4>${response.imdbRating}</h4>
-                </div>
-                <div class="description">
-                  <span>${response.Rated}</span>
-                  <span>${response.Year}</span>
-                  <span>${response.Runtime}</span>
-                </div>
-                <div class="genero-container">
-                  ${arraygenero.map(genero => `<p class="genero">${genero}</p>`).join('')}
-                </div>
-              </div>
-            </div>
-          </div>
-          <div id="description2">
-            <h2>Descrição</h2>
-            <span>${response.Plot}</span>
-          </div>
-          <div id="atores">
-            <h2>Atores</h2>
-            <span>${response.Actors}</span>
-          </div>`
-      } else {
-        res.innerHTML = 'Filme não encontrado'
-      }
-    }).catch(() => {
-      res.innerHTML = 'Erro ao obter os dados do filme'
-    })
-  }
-})
+});
